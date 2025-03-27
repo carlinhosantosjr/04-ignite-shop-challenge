9f17747a034e1bb2659cb0f3c1f4e3ebb0619333
carlinhosantosjr <carlinhosantosjr@hotmail.com>
2025-03-27 20:34:35 -0300

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-};
 
-export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+    ],
+  },
+
+}
+
+export default nextConfig
