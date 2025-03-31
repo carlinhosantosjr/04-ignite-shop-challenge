c9cf7b69dbb06fabaf9fcc79adc54ccf638c1542
carlinhosantosjr <carlinhosantosjr@hotmail.com>
2025-03-31 15:58:27 -0300

    Completed project commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import { useKeenSlider } from 'keen-slider/react'
+import Head from 'next/head'
 import { HomeContainer, Product } from '../styles/pages/home'
 
 import 'keen-slider/keen-slider.min.css'
@@ -27,27 +28,34 @@ export default function Home({ products }:HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Link
-            key={product.id}
-            href={`/product/${product.id} `}
-          >
-            <Product
-              className="keen-slider__slide"
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.id} `}
+              prefetch={false}
             >
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      },
-      )}
-    </HomeContainer>
+              <Product
+                className="keen-slider__slide"
+              >
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        },
+        )}
+      </HomeContainer>
+    </>
   )
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,8 +4,11 @@ import {
   ProductDetails,
 } from '../../styles/pages/product'
 
-import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import Stripe from 'stripe'
 import { stripe } from '../../lib/stripe'
 
@@ -16,33 +19,76 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
-  // const { query } = useRouter()
+  const [
+    isCreatingCheckoutSession,
+    setIsCreatingCheckoutSession,
+  ] = useState(false)
+
+  async function handleBuyButton() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      setIsCreatingCheckoutSession(false)
+
+      alert(`${err} Falha ao redirecionar ao checkout!`)
+    } finally {
+      setIsCreatingCheckoutSession(false)
+    }
+  }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
-      </ImageContainer>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImageContainer>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>{product.description}</p>
+          <p>{product.description}</p>
 
-        <button>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+          <button
+            disabled={isCreatingCheckoutSession}
+            onClick={handleBuyButton}
+          >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
-export const getStaticProps: GetStaticProps<any, { id: string }> =
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_S0doHSg9PsUcbh' },
+      },
+    ],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps<unknown, { id: string }> =
   async ({ params }) => {
     const productId = params
       ? params.id
@@ -67,6 +113,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> =
               }).format(price.unit_amount / 100)
             : 0,
           description: product.description,
+          defaultPriceId: price.id,
         },
       },
       revalidate: 60 * 60 * 1, // 1 hours
